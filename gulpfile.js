970f4bd8bfa66607d5bf155eaf4f6b71726d06e2
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-07-11 13:36:22 +0300

    bower dependencies compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
     watch = require('gulp-watch'),
-    del = require('rimraf'),
-    rename = require('gulp-rename'), // not used
-    concat = require('gulp-concat'), // not used
+    del = require('del'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     jade = require('gulp-jade'),
@@ -15,7 +16,9 @@ var gulp = require('gulp'),
     cssmin = require('gulp-minify-css'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
-    browserSync = require("browser-sync"),
+    browserSync = require('browser-sync'),
+    mainBowerFiles = require('main-bower-files'),
+    filter = require('gulp-filter'),
     reload = browserSync.reload;
 
 var path = {
@@ -43,6 +46,7 @@ var path = {
   clean: './dist'
 };
 
+// Browser Sync
 var config = {
   server: {
     baseDir: "./dist"
@@ -53,7 +57,6 @@ var config = {
   logPrefix: "Frontend_Devil"
 };
 
-// Browser Sync
 gulp.task('webserver', function() {
     browserSync(config);
 });
@@ -66,6 +69,7 @@ gulp.task('clean', function(cb) {
 // Images
 gulp.task('image:build', function() {
   gulp.src(path.src.img)
+    .pipe(plumber())
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
@@ -79,12 +83,14 @@ gulp.task('image:build', function() {
 // Fonts
 gulp.task('fonts:build', function() {
   gulp.src(path.src.fonts)
+    .pipe(plumber())
     .pipe(gulp.dest(path.build.fonts))
 });
 
 // HTML
 gulp.task('html:build', function() {
   gulp.src(path.src.html)
+    .pipe(plumber())
     .pipe(rigger())
     .pipe(jade({pretty: true}))
     .pipe(gulp.dest(path.build.html))
@@ -94,11 +100,13 @@ gulp.task('html:build', function() {
 // Styles
 gulp.task('style:build', function() {
   gulp.src(path.src.style)
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(rename('style.css'))
     .pipe(gulp.dest(path.build.css))
     .pipe(reload({stream: true}));
 });
@@ -106,41 +114,76 @@ gulp.task('style:build', function() {
 // Scripts
 gulp.task('js:build', function() {
   gulp.src(path.src.js)
+    .pipe(plumber())
     .pipe(rigger())
     .pipe(sourcemaps.init())
+    .pipe(coffee())
     .pipe(uglify())
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest(path.build.js))
     .pipe(reload({stream: true}));
 });
 
+// Vendors
+var vendors = mainBowerFiles({
+  includeDev: true,
+  paths: {
+    bowerDirectory: 'lib/',
+    bowerrc: '.bowerrc',
+    bowerJson: 'bower.json'
+  }
+});
+
+gulp.task('scripts:vendor', function () {
+  gulp.src(vendors)
+    .pipe(plumber())
+    .pipe(filter('**/*.js'))
+    .pipe(uglify())
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(path.build.js))
+});
+
+gulp.task('styles:vendor', function () {
+  gulp.src(vendors)
+    .pipe(plumber())
+    .pipe(filter('**/*.css'))
+    .pipe(cssmin())
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(path.build.css))
+});
+
+gulp.task('vendor', [
+  'scripts:vendor',
+  'styles:vendor'
+]);
+
 // Create dist
 gulp.task('build', [
-    'html:build',
-    'js:build',
-    'style:build',
-    'fonts:build',
-    'image:build'
+  'html:build',
+  'js:build',
+  'style:build',
+  'fonts:build',
+  'image:build'
 ]);
 
 // Watch
 gulp.task('watch', function(){
-    watch([path.watch.html], function(event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.style], function(event, cb) {
-        gulp.start('style:build');
-    });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
-    });
-    watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
-    });
+  watch([path.watch.html], function(event, cb) {
+    gulp.start('html:build');
+  });
+  watch([path.watch.style], function(event, cb) {
+    gulp.start('style:build');
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start('js:build');
+  });
+  watch([path.watch.img], function(event, cb) {
+    gulp.start('image:build');
+  });
+  watch([path.watch.fonts], function(event, cb) {
+    gulp.start('fonts:build');
+  });
 });
 
 // Default task
-gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('default', ['build', 'webserver', 'vendor', 'watch']);
