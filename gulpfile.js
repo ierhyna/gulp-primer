2b3af3606125d676eed57785badebb1290dacaf2
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-07-11 15:16:17 +0300

    fix errors

--- gulpfile.js
+++ gulpfile.js
@@ -169,19 +169,19 @@ gulp.task('build', [
 // Watch
 gulp.task('watch', function(){
   watch([path.watch.html], function(event, cb) {
-    gulp.start('html:build');
+    gulp.start('html:build')
   });
   watch([path.watch.style], function(event, cb) {
-    gulp.start('style:build');
+    gulp.start('style:build')
   });
   watch([path.watch.js], function(event, cb) {
-    gulp.start('js:build');
+    gulp.start('js:build')
   });
   watch([path.watch.img], function(event, cb) {
-    gulp.start('image:build');
+    gulp.start('image:build')
   });
   watch([path.watch.fonts], function(event, cb) {
-    gulp.start('fonts:build');
+    gulp.start('fonts:build')
   });
 });
 
