6ea7b9e3a4d03c3317d30efe319ee873ce22f280
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-10-09 19:36:16 +0300

    getting things easier

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
-# Primer Template
+# Gulp Primer
 
-Primer is a starter template for web site frontend. It includes:
-
-- Jade for HTML
-- SCSS and Bourbon for CSS
-- Coffee Script for JavaScript
-- Bower as dependencies manager
-- Gulp as task runner
+It is a quick start Gulp boilerplate for front end project.
 
 Usage:
 
 1. `npm install` to install packages
 1. `bower install` to install vendors
-1. `gulp build` to compile HTML, CSS and JS from sources
-1. `gulp watch` to watch the changes
 1. `gulp` to watch, compile, and start a webserver
 
 Structure:
 
-- assets: contains Jade, SCSS and Coffee Script files
-- dist: compiled HTML, CSS, JS files
-- lib: bower components
+- sass: SCSS files
+- stylesheets: CSS files
 
 Author: [Irina Sokolovskaya](http://github.com/ierhyna)
--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,40 @@
 'use strict';
 
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    watch = require('gulp-watch'),
-    del = require('del'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    jade = require('gulp-jade'),
-    coffee = require('gulp-coffee'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rigger = require('gulp-rigger'),
     cssmin = require('gulp-minify-css'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
     browserSync = require('browser-sync'),
-    mainBowerFiles = require('main-bower-files'),
-    filter = require('gulp-filter'),
     reload = browserSync.reload;
 
 var path = {
   build: {
-    html: 'dist/',
-    js: 'dist/js/',
-    css: 'dist/css/',
-    img: 'dist/img/',
-    fonts: 'dist/fonts/'
+    // js: 'dist/js/'
+    css: 'stylesheets/'
   },
   src: {
-    html: 'assets/jade/*.jade', 
-    js: 'assets/coffee/main.coffee',
-    style: 'assets/scss/main.scss',
-    img: 'assets/img/**/*.*',
-    fonts: 'assets/fonts/**/*.*'
+    html: './*.html', 
+    js: 'js/*.js',
+    style: 'sass/main.scss'
   },
   watch: {
-    html: 'assets/jade/*.jade',
-    js: 'assets/coffee/**/*.js',
-    style: 'assets/scss/**/*.scss',
-    img: 'assets/img/**/*.*',
-    fonts: 'assets/fonts/**/*.*'
+    html: './*.html', 
+    js: 'js/*.js',
+    style: 'sass/**/*.scss'
   },
-  clean: './dist'
+  clean: 'stylesheets/'
 };
 
 // Browser Sync
 var config = {
   server: {
-    baseDir: "./dist"
+    baseDir: "./"
   },
-  tunnel: true,
+  tunnel: false,
   host: 'localhost',
   port: 9000,
   logPrefix: "Frontend_Devil"
@@ -66,46 +49,18 @@ gulp.task('clean', function(cb) {
   del(path.clean, cb)
 });
 
-// Images
-gulp.task('image:build', function() {
-  gulp.src(path.src.img)
-    .pipe(plumber())
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()],
-      interlaced: true
-    }))
-    .pipe(gulp.dest(path.build.img))
-    .pipe(reload({stream: true}));
-});
-
-// Fonts
-gulp.task('fonts:build', function() {
-  gulp.src(path.src.fonts)
-    .pipe(plumber())
-    .pipe(gulp.dest(path.build.fonts))
-});
-
 // HTML
 gulp.task('html:build', function() {
   gulp.src(path.src.html)
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest(path.build.html))
     .pipe(reload({stream: true}));
 });
 
 // Styles
 gulp.task('style:build', function() {
   gulp.src(path.src.style)
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(sass( {errLogToConsole: true} ))
     .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(sourcemaps.write('./maps'))
     .pipe(rename('style.css'))
     .pipe(gulp.dest(path.build.css))
     .pipe(reload({stream: true}));
@@ -114,56 +69,16 @@ gulp.task('style:build', function() {
 // Scripts
 gulp.task('js:build', function() {
   gulp.src(path.src.js)
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(sourcemaps.init())
-    .pipe(coffee())
-    .pipe(uglify())
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest(path.build.js))
+    // .pipe(uglify())
+    // .pipe(gulp.dest(path.build.js))
     .pipe(reload({stream: true}));
 });
-
-// Vendors
-var vendors = mainBowerFiles({
-  includeDev: true,
-  paths: {
-    bowerDirectory: 'lib/',
-    bowerrc: '.bowerrc',
-    bowerJson: 'bower.json'
-  }
-});
-
-gulp.task('scripts:vendor', function () {
-  gulp.src(vendors)
-    .pipe(plumber())
-    .pipe(filter('**/*.js'))
-    .pipe(uglify())
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(path.build.js))
-});
-
-gulp.task('styles:vendor', function () {
-  gulp.src(vendors)
-    .pipe(plumber())
-    .pipe(filter('**/*.css'))
-    .pipe(cssmin())
-    .pipe(concat('vendor.css'))
-    .pipe(gulp.dest(path.build.css))
-});
-
-gulp.task('vendor', [
-  'scripts:vendor',
-  'styles:vendor'
-]);
 
 // Create dist
 gulp.task('build', [
   'html:build',
   'js:build',
-  'style:build',
-  'fonts:build',
-  'image:build'
+  'style:build'
 ]);
 
 // Watch
@@ -177,13 +92,8 @@ gulp.task('watch', function(){
   watch([path.watch.js], function(event, cb) {
     gulp.start('js:build')
   });
-  watch([path.watch.img], function(event, cb) {
-    gulp.start('image:build')
-  });
-  watch([path.watch.fonts], function(event, cb) {
-    gulp.start('fonts:build')
-  });
 });
 
 // Default task
-gulp.task('default', ['build', 'vendor', 'watch', 'webserver']);
+gulp.task('default', ['build', 'webserver', 'watch']);
+
